01afcddb1cb0505768b3741f73f35fdb04280bda
heba ali <117442802+hebaali6001@users.noreply.github.com>
2023-04-21 01:36:47 +0200

    Library

---
+++ COTS/LIB/Stdtypes.h
@@ -0,0 +1,32 @@
+/*
+ * Stdtypes.h
+ *
+ *  Created on: Dec 1, 2022
+ *      Author: JUMBO
+ */
+
+#ifndef STDTYPES_H_
+#define STDTYPES_H_
+
+
+typedef unsigned char u8;
+typedef unsigned short int u16;
+typedef unsigned int u32;
+typedef unsigned long int u64;
+
+
+typedef signed char s8;
+typedef signed short int s16;
+typedef signed int s32;
+typedef signed long long int s64;
+
+
+typedef float f32;
+
+typedef double f64;
+
+typedef long double f128;
+
+
+
+#endif /* STDTYPES_H_ */
